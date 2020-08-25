5e174adab539d8b2cb4dc2c846c8b7f55437eb48
XiaomengYuan9709 <41124205+XiaomengYuan9709@users.noreply.github.com>
2020-08-25 14:31:07 -0400

    update for data on 2-24-2020
    Add total on 2-24-2020 for all locations in Geo.csv.
    update column new on data 2-24-2020, 9:00AM for all locations in Geo.csv.

--- Kobs.R
+++ Kobs.R
@@ -16,7 +16,9 @@ data$level <- rep(NA, 1114)
 for (i in 1:1114) {
            ifelse(data$ID[i]==1 || data$ID[i]==4 || data$ID[i]==5 || data$ID[i]==7 || 
                   data$ID[i]==11 || data$ID[i]==15 || data$ID[i]==16 ||data$ID[i]==17, 
-                  data$level[i] <- "city", data$level[i] <- "province")}
+                  data$level[i] <- "city", data$level[i] <- "province")
+                  }
+
 data$level <- as.factor(data$level)
 summary(data$level)
 
@@ -24,35 +26,46 @@ summary(data$level)
 data$date <- as.Date(data$date)
 
 
-# delete observations before 2020-02-25
-data1 <- data[data$date >= "2020-02-25", ]
+# delete observations with time=16:00, as most obs w/ time= 0:00 and 
+# it's closer to 9:00.
+data1 <- data[data$time!= "16:00", ]
 
 # delete observations from Airport
 data2 <- data1[data1$ID!=0, ]
-row.names(data2) <- 1:952
-data2$diff <- c(NA, diff(data2$total))
+row.names(data2) <- 1:1020
 
+# create a column to represent difference between row i and i-1
+data2$diff <- c(NA, diff(data2$total))
 # check if new (newly confirmed) is equal to diff in total
 dv <- which(data2$new!= data2$diff)
-dv # rows where new != diff
-# [1]   1   2  57  61 113 169 174 175 203 225 229 230 281 337 393 449 450 455
-# [19] 505 561 617 673 729 730 785 841 842 897
-
-# check these rows
+dv # rows where new != diff, a few cases 
+# [1]   5   6  69 190 191 219 245 249 250 305 425 485 486 491 785 786 905 906
+# check these rows (not verified yet as of 08-25-2020)
 dr <- data2[dv, ]
 
+
 ###plot to check for if values looks correct
-p <- ggplot(data3, aes(x=date, y=total)) + 
+p <- ggplot(data2, aes(x=date, y=total)) + 
   geom_line() +
   facet_wrap(~ID)
 p
 
+p1 <- ggplot(data2, aes(x=date, y=new)) + 
+  geom_line() +
+  facet_wrap(~ID)
+p1
 ########check if total is non-decreasing
-x1 <-data3[data3$ID==1, ]
-which(diff(x1$total)<0)
-
-# from 2020-03-09, we can see total decreased to 96,
+nd <-which(diff(data2$total)<0)
+nd
+# [1]  13  17 249 258 297 304 374 785 793
+# the above 9 rows were checked and found to be conformed with original data source on KCDC
+# For one row listed above (17): from 2020-03-09, we can see total decreased to 96,
 # check CDC website we found this note:
 #" *There was one deceased case in Busan, 
 # but the local government in charge of the case changed from Busan to Gyeongbuk."
 # on (https://www.cdc.go.kr/board/board.es?mid=a30402000000&bid=0030&act=view&list_no=366490&tag=&nPage=33)
+
+####### Check for NA
+summary(data2$new)
+summary(data2$total)
+which(is.na(data2$total)) #every location missing 2-21 to 2-23
